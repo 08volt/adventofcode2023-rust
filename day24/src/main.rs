3bf631d0ada82944d97e0942348ab0b8c1cfcbe7
Enrico <enrico.voltan@mail.polimi.it>
2023-12-25 08:27:31 +0100

    day24 part2 wip

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -49,31 +49,51 @@ fn example() {
     let part1 = solve_day_24_part1(EXAMPLE_DATA_1, (7_f64, 27_f64));
     assert_eq!(part1, 2);
 
-    // let part2 = solve_day_24_part2(EXAMPLE_DATA_1, (0, 1), (22, 21));
-    // assert_eq!(part2, 154);
+    let part2 = solve_day_24_part2(EXAMPLE_DATA_1);
+    assert_eq!(part2, 47);
 }
+
+#[derive(Debug)]
 struct Stone {
     pos: (f64, f64, f64),
     v: (f64, f64, f64),
 }
 
 impl Stone {
-    fn cross_at(&self, other: &Self) -> Option<(f64, f64)> {
-        println!("\n{}", Into::<String>::into(self));
-        println!("{}", Into::<String>::into(other));
+    fn is_parallel(&self, other: &Self) -> bool {
+        (self.v.0 * other.v.1 / (self.v.1 * other.v.0) - 1_f64) == 0_f64
+    }
 
-        if (self.v.0 * other.v.1 / (self.v.1 * other.v.0) - 1_f64) == 0_f64 {
-            return None;
+    fn cross_at(&self, other: &Self) -> Option<(f64, f64)> {
+        // println!("\n{}", Into::<String>::into(self));
+        // println!("{}", Into::<String>::into(other));
+
+        if self.is_parallel(other) {
+            return None; // parallele
         }
 
-        let y = ((self.pos.1 * self.v.0 / self.v.1 + self.pos.0 - other.pos.0) * other.v.1
-            / other.v.0
-            + other.pos.1)
-            / (self.v.0 * other.v.1 / (self.v.1 * other.v.0) - 1_f64);
+        let a = self.pos.0;
+        let b = self.v.0;
+        let c = self.pos.1;
+        let d = self.v.1;
 
-        let x = y * self.v.0 / self.v.1 - self.pos.1 * self.v.0 / self.v.1 + self.pos.0;
+        let e = other.pos.0;
+        let f = other.v.0;
+        let g = other.pos.1;
+        let h = other.v.1;
 
-        Some((x, y))
+        let x = (b * f * g + a * d * f - h * e * b - c * b * f) / (d * f - h * b);
+        let y = c + (d / b) * (x - a);
+
+        if self.is_past((x, y)) || other.is_past((x, y)) {
+            None
+        } else {
+            Some((x, y))
+        }
+    }
+
+    fn is_past(&self, pos: (f64, f64)) -> bool {
+        (pos.0 - self.pos.0) * self.v.0 < 0_f64 || (pos.1 - self.pos.1) * self.v.1 < 0_f64
     }
 }
 
@@ -122,7 +142,7 @@ fn solve_day_24_part1(input: &str, test_area: (f64, f64)) -> u64 {
                     && cross.1 > test_area.0
                     && cross.1 < test_area.1
                 {
-                    println!("FOUND! {:?}", cross);
+                    // println!("FOUND! {:?}", cross);
                     count += 1;
                 }
             }
@@ -132,6 +152,39 @@ fn solve_day_24_part1(input: &str, test_area: (f64, f64)) -> u64 {
     count
 }
 
-// fn solve_day_24_part2(input: &str, start_pos: (f64, f64), end_pos: (f64, f64)) -> u64 {
-//     0
-// }
+fn solve_day_24_part2(input: &str) -> u64 {
+    let stones: Vec<Stone> = input.lines().map(|l| Stone::from_str(l).unwrap()).collect();
+
+    let mut parallels = Vec::<(&Stone, &Stone)>::new();
+
+    while parallels.len() < 2 {
+        for (i, s) in stones.iter().enumerate() {
+            println!("\n{}", Into::<String>::into(s));
+            for s2 in stones.iter().skip(i + 1) {
+                if s.is_parallel(s2) {
+                    println!("FOUND {:?}", (s, s2));
+                    parallels.push((s, s2));
+                    break;
+                }
+            }
+        }
+    }
+
+    let x1 = parallels[0].0.pos.0;
+    let y1 = parallels[0].0.pos.1;
+    let z1 = parallels[0].0.pos.2;
+    let x2 = parallels[0].1.pos.0;
+    let y2 = parallels[0].1.pos.1;
+    let z2 = parallels[0].1.pos.1;
+    let x3 = parallels[0].1.pos.0 + parallels[0].1.v.0;
+    let y3 = parallels[0].1.pos.1 + parallels[0].1.v.1;
+    let z3 = parallels[0].1.pos.2 + parallels[0].1.v.2;
+
+    let a = (y2 - y1) * (z3 - z1) - (z2 - z1) * (y3 - y1);
+    let b = (z2 - z1) * (x3 - x1) - (x2 - x1) * (z3 - z1);
+    let c = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
+
+    let d = a * x1 + b * x2 + c * z1;
+
+    0
+}
